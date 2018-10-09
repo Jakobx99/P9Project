9d57565581d9cb6e675de11f1a4a33db2d1ec879
Jakob Holgersen <Jakobholgersen@gmail.com>
2018-10-09 13:58:05 +0200

    Added fragments and support for database in these

--- DiabetesInformationApplication/app/src/main/java/hci923e18/diabetesinformationapplication/FrontPageActivity.java
+++ DiabetesInformationApplication/app/src/main/java/hci923e18/diabetesinformationapplication/FrontPageActivity.java
@@ -1,5 +1,8 @@
 package hci923e18.diabetesinformationapplication;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -12,38 +15,24 @@ import hci923e18.database.Person;
 
 public class FrontPageActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    fragmentTransaction.replace(R.id.framelayout, new HomeFragment()).commit();
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-
-
-                    //Eksemple på at gemme et person object
-                    Person person = new Person("Jhon", "Johnson", 30);
-                    person.save();
-                    Toast.makeText(getApplicationContext(), "SAVING", Toast.LENGTH_LONG).show();
-                    /////
-
-
+                    fragmentTransaction.replace(R.id.framelayout, new DataFragment()).commit();
                     return true;
                 case R.id.navigation_notifications:
-
-                    //Eksemple på at hente et person object
-                    Person local;
-                    local = Person.find(Person.class, "age = ?", "30").get(0);
-                    //////
-
-
-                    mTextMessage.setText(local.firstname + " " + local.lastname + " " + local.age);
+                    fragmentTransaction.replace(R.id.framelayout, new InformationFragment()).commit();
                     return true;
             }
             return false;
@@ -55,9 +44,16 @@ public class FrontPageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_front_page);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.framelayout, new HomeFragment()).commit();
     }
 
 }
+
+
+
