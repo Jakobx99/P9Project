259601baccf13d69fcd265cd8e04c2bc1dbf8efc
Jakob Holgersen <Jakobholgersen@gmail.com>
2018-10-23 09:22:49 +0200

    Fixed keyboard on settings page, added method to add default food items to database on initial launch

--- DiabetesInformationApplication/app/src/main/java/hci923e18/diabetesinformationapplication/FrontPageActivity.java
+++ DiabetesInformationApplication/app/src/main/java/hci923e18/diabetesinformationapplication/FrontPageActivity.java
@@ -12,6 +12,9 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.orm.SugarRecord;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import hci923e18.database.Food;
@@ -68,7 +71,6 @@ public class FrontPageActivity extends AppCompatActivity {
         List<Food> _food = Food.listAll(Food.class);
         if (_food.size() == 0)
         {
-            Toast.makeText(this, "FIRST", Toast.LENGTH_LONG).show();
             populateDB();
         }
         else{
@@ -108,24 +110,28 @@ public class FrontPageActivity extends AppCompatActivity {
     }
 
     /**
-     * Method to populate Database with default Food items
+     * Method to populate Database with default Food items first time the app is launched
      */
     private void populateDB(){
 
         //http://maddata.dk/
         // Food(String name, Double carbohydrate, Double protein, Double fiber, Double sugar)
-        Food cola = new Food("Cola", 10.6, 0.1, 0.0, 10.6);
-        Food colalight = new Food("Cola light", 0.0, 0.0,0.0,0.0);
-        Food mælk = new Food("Arla Letmælk", 4.7, 3.5, 0.0,4.7);
-        Food ris = new Food("Ris", 28.0, 2.7,0.4, 0.1);
-        Food kartofler = new Food("Kartofler", 17.0, 2.0, 2.2, 0.8);
-        Food pasta = new Food("Pasta", 75.0, 13.0, 3.2, 2.7);
-        Food kylling = new Food("Kyllingebryst", 0.0, 21.5, 0.0, 0.0);
-        Food oksefars = new Food("Hakket oksekød", 0.0, 19.3, 0.0, 0.0);
-        Food grisefars = new Food("Hakket Svinekød", 0.0, 21.2, 0.0, 0.0);
-        Food rugbrød = new Food("Mørkt rugbrød", 47.4, 5.7, 8.6, 0.0);
+        List<Food> foods = new ArrayList<>();
+        foods.add(new Food("Cola", 10.6, 0.1, 0.0, 10.6));
+        foods.add(new Food("Cola light", 0.0, 0.0,0.0,0.0));
+        foods.add(new Food("Arla Letmælk", 4.7, 3.5, 0.0,4.7));
+        foods.add(new Food("Ris", 28.0, 2.7,0.4, 0.1));
+        foods.add(new Food("Kartofler", 17.0, 2.0, 2.2, 0.8));
+        foods.add(new Food("Pasta", 75.0, 13.0, 3.2, 2.7));
+        foods.add(new Food("Kyllingebryst", 0.0, 21.5, 0.0, 0.0));
+        foods.add(new Food("Hakket oksekød", 0.0, 19.3, 0.0, 0.0));
+        foods.add(new Food("Hakket Svinekød", 0.0, 21.2, 0.0, 0.0));
+        foods.add(new Food("Mørkt rugbrød", 47.4, 5.7, 8.6, 0.0));
 
-        //Add to db plz
+        //Add to database
+        SugarRecord.saveInTx(foods);
+
+
 
     }
 
