641d319ea30e31fc008f6354d73461c7c2a7d3cf
Jakob Holgersen <Jakobholgersen@gmail.com>
2018-11-20 15:27:00 +0100

    Added method to create header and child list

--- DiabetesInformationApplication/app/src/main/java/hci923e18/utility/FetchBloodMeasurements.java
+++ DiabetesInformationApplication/app/src/main/java/hci923e18/utility/FetchBloodMeasurements.java
@@ -1,5 +1,6 @@
 package hci923e18.utility;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
@@ -11,20 +12,29 @@ import hci923e18.database.BloodGlucoseMeasurements;
 public class FetchBloodMeasurements {
 
     public void fetchData(Calendar startDate, Calendar endDate){
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 
-        //TODO Skal sætter som global variabler
+        //TODO Skal sætter som global variabler/////////////////////////////////
         List<String> listHead = new ArrayList<>();
-        HashMap<String, BloodGlucoseMeasurements> listChild;
+        HashMap<String, BloodGlucoseMeasurements> listChild = null;
+        ///////////////////////////////////////////////////////////////////////7
 
         //Fetch all measurements from/to date
         List<BloodGlucoseMeasurements> l = new ArrayList<>();
 
         l.addAll(BloodGlucoseMeasurements.find(BloodGlucoseMeasurements.class, "_date >= ? and _date <= ?", Long.toString(startDate.getTimeInMillis()), Long.toString(endDate.getTimeInMillis())));
 
+        Calendar currentCalendar = startDate;
+        listHead.add(sdf.format(currentCalendar));
+
         for (BloodGlucoseMeasurements b:l) {
-            
+            if (!sdf.format(currentCalendar.getTimeInMillis()).equals(sdf.format(b.getDate().getTimeInMillis()))){
+                currentCalendar = b.getDate();
+                listHead.add(sdf.format(currentCalendar.getTimeInMillis()));
+                listChild.put(sdf.format(currentCalendar.getTimeInMillis()), b);
+            } else {
+                listChild.put(sdf.format(currentCalendar.getTimeInMillis()), b);
+            }
         }
-
-
     }
 }
